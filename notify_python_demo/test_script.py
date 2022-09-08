28bcaa8468c3854cffa70280d88452749d38fbad
Ben Klaas <ben.klaas@gsa.gov>
2022-09-08 17:05:17 -0500

    add black to dev deps

--- notify_python_demo/test_script.py
+++ notify_python_demo/test_script.py
@@ -1,7 +1,8 @@
-import os 
+import os
+
 from dotenv import load_dotenv
-from notifications_python_client.notifications import NotificationsAPIClient
 from pprint import pprint
+from notifications_python_client.notifications import NotificationsAPIClient
 
 
 load_dotenv()
@@ -10,12 +11,12 @@ load_dotenv()
 base_url = "http://localhost:6011"
 
 # STUFF FROM .env file
-user_api_key = os.environ.get('USER_API_KEY')
-admin_api_key = os.environ.get('ADMIN_API_KEY')
-iss_uid = os.environ.get('ISS_UUID')
-service_name = os.environ.get('SERVICE_NAME')
-template_id = os.environ.get('TEMPLATE_ID')
-phone_number = os.environ.get('PHONE_NUMBER')
+user_api_key = os.environ.get("USER_API_KEY")
+admin_api_key = os.environ.get("ADMIN_API_KEY")
+iss_uid = os.environ.get("ISS_UUID")
+service_name = os.environ.get("SERVICE_NAME")
+template_id = os.environ.get("TEMPLATE_ID")
+phone_number = os.environ.get("PHONE_NUMBER")
 
 concat_api_key = "_".join([service_name, iss_uid, user_api_key])
 
@@ -25,11 +26,7 @@ notifications_client = NotificationsAPIClient(concat_api_key, base_url=base_url)
 response = notifications_client.send_sms_notification(
     phone_number=phone_number,
     template_id=template_id,
-    personalisation={
-        "day_of_week": "Wednesday",
-        "colour": "Purple"
-    }
+    personalisation={"day_of_week": "Wednesday", "colour": "Purple"},
 )
 
 pprint(response)
-
