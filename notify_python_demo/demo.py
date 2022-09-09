168e7d8db65382babc4adc0757038e8d5f68cbc0
Ben Klaas <ben.klaas@gsa.gov>
2022-09-09 16:17:11 -0500

    revert test_script.py, start demo.py

---
+++ notify_python_demo/demo.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from pprint import pprint
+from notifications_python_client.notifications import NotificationsAPIClient
+
+
+load_dotenv()
+
+# hard-code API URL to local dev location
+base_url = "http://localhost:6011"
+
+# STUFF FROM .env file
+user_api_key = os.environ.get("USER_API_KEY")
+admin_api_key = os.environ.get("ADMIN_API_KEY")
+iss_uid = os.environ.get("ISS_UUID")
+service_name = os.environ.get("SERVICE_NAME")
+template_id = os.environ.get("TEMPLATE_ID")
+phone_number = os.environ.get("PHONE_NUMBER")
+
+concat_api_key = "_".join([service_name, iss_uid, user_api_key])
+
+# must pass in base_url, as the default is notify.uk's production URL
+notifications_client = NotificationsAPIClient(concat_api_key, base_url=base_url)
+
+response = notifications_client.send_sms_notification(
+    phone_number=phone_number,
+    template_id=template_id,
+    personalisation={"day_of_week": "Wednesday", "colour": "Purple"},
+)
+
+pprint(response)
--- notify_python_demo/test_script.py
+++ notify_python_demo/test_script.py
@@ -1,32 +1,25 @@
-import os
-
+import os 
 from dotenv import load_dotenv
-from pprint import pprint
 from notifications_python_client.notifications import NotificationsAPIClient
 
 
 load_dotenv()
+api_key = os.environ.get('USER_API_KEY')
+admin_api_key = os.environ.get('ADMIN_API_KEY')
+service_id = os.environ.get('ISS_UID')
+service_key_name = os.environ.get('SERVICE_NAME')
 
-# hard-code API URL to local dev location
-base_url = "http://localhost:6011"
-
-# STUFF FROM .env file
-user_api_key = os.environ.get("USER_API_KEY")
-admin_api_key = os.environ.get("ADMIN_API_KEY")
-iss_uid = os.environ.get("ISS_UUID")
-service_name = os.environ.get("SERVICE_NAME")
-template_id = os.environ.get("TEMPLATE_ID")
-phone_number = os.environ.get("PHONE_NUMBER")
-
-concat_api_key = "_".join([service_name, iss_uid, user_api_key])
-
+concat_api_key = "_".join([service_key_name, service_id, api_key])
 # must pass in base_url, as the default is notify.uk's production URL
-notifications_client = NotificationsAPIClient(concat_api_key, base_url=base_url)
+notifications_client = NotificationsAPIClient(concat_api_key, base_url="http://localhost:6011")
 
 response = notifications_client.send_sms_notification(
-    phone_number=phone_number,
-    template_id=template_id,
-    personalisation={"day_of_week": "Wednesday", "colour": "Purple"},
+    phone_number="18016525984",
+    template_id="8ccb6087-abf9-469e-bce5-9a3b361dd4c2",
+    personalisation={
+        "day_of_week": "Wednesday",
+        "colour": "Purple"
+    }
 )
 
-pprint(response)
+print(response)
