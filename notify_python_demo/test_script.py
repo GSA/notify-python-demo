86bee2900d5010a2350c67a4aac0024f7da6801c
Carly Jugler <carjug@gmail.com>
2022-09-09 12:31:43 -0600

    Add commandline args and validation for sending sms

--- notify_python_demo/test_script.py
+++ notify_python_demo/test_script.py
@@ -1,5 +1,7 @@
-import os 
+import os
+import sys
 import argparse
+import logging
 from dotenv import load_dotenv
 from notifications_python_client.notifications import NotificationsAPIClient
 
@@ -10,13 +12,28 @@ def parse_args() -> argparse.Namespace:
     return argparse object
     """
     parser = argparse.ArgumentParser(description='Options for testing python notify client')
-    parser.add_argument('--send-sms', type=bool, help='send sms')
-    parser.add_argument('--send-email', type=bool, help='send email')
+    parser.add_argument('--send_sms', type=bool, help='send sms')
+    parser.add_argument('--send_email', type=bool, help='send email')
     parser.add_argument('--number', type=str, help='phone number to send notification')
     parser.add_argument('--email', type=str, help='email address to send notification')
-
     args = parser.parse_args()
+
     return args
+
+def check_and_send_sms(notifications_client, args):
+    if not args.number:
+        logging.error("Cannot send sms without phone number")
+    if args.number:
+        # validate phone number?
+        response = notifications_client.send_sms_notification(
+            phone_number=args.number,
+            template_id="8ccb6087-abf9-469e-bce5-9a3b361dd4c2",
+            personalisation={
+                "day_of_week": "Wednesday",
+                "colour": "Purple"
+            }
+        )
+        print(response)
 
 
 def main():
@@ -26,21 +43,18 @@ def main():
     service_id = os.environ.get('ISS_UID')
     service_key_name = os.environ.get('SERVICE_NAME')
     concat_api_key = "_".join([service_key_name, service_id, api_key])
+    base_url = "http://localhost:6011"
+
+    # must pass in base_url, as the default is notify.uk's production URL
+    notifications_client = NotificationsAPIClient(concat_api_key, base_url=base_url)
     
     args = parse_args()
-    # must pass in base_url, as the default is notify.uk's production URL
-    notifications_client = NotificationsAPIClient(concat_api_key, base_url="http://localhost:6011")
+    if len(sys.argv) == 0:
+        print("Please pass in some arguments to run the script.")
 
-    response = notifications_client.send_sms_notification(
-        phone_number="18016525984",
-        template_id="8ccb6087-abf9-469e-bce5-9a3b361dd4c2",
-        personalisation={
-            "day_of_week": "Wednesday",
-            "colour": "Purple"
-        }
-    )
-
-    print(response)
+    # todo: csv check/load here before others
+    if args.send_sms:
+        check_and_send_sms(notifications_client, args)
 
 if __name__ == '__main__':
     main()
